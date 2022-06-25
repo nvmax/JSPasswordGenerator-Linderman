c5b9b6bf4dfdb0f55fe7ba2aa11a45308eeef760
Jerrod Linderman <nvmaxx@gmail.com>
2022-06-25 15:00:10 -0500

    cleaned up spacing, added comments

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ console.log(numberChar)
 var specialChar = String.fromCharCode(...Array(123).keys()).slice(32,48)
     .concat(String.fromCharCode(...Array(123).keys()).slice(59,65))
 console.log(specialChar)
-
 var passwordLength;
 var checklowerCase;
 var checkUpperCase;
@@ -35,16 +34,18 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
 // password length function and check//
 function lengthCheck() {
   passwordLength = prompt("How many characters would you like your password to be?");
+//checks if password is greater than 8
    if (passwordLength < 8) {
     alert("Password must be at least 8 characters long.");
     lengthCheck();
+// checks if password is less than 128
    }else if (passwordLength > 128) {
     alert("Password must be less than 128 characters long.");
     lengthCheck();
+// checks if there is nothing entered
     } else if (isNaN(passwordLength)) {
     alert("Password must be a number.");
     lengthCheck();
@@ -56,15 +57,19 @@ function lengthCheck() {
 function lowercaseCheck(){
   checklowerCase = prompt("Do you want lowercase in your password? \n(Yes or No)");
     checklowerCase = checklowerCase.toLowerCase();
+// asks for  response of yes or no
     if (checklowerCase === null || checklowerCase === ""){
       alert("Please answer Yes or No");
       lowercaseCheck();
+// if yes, checklowerCase is true
     }else if (checklowerCase === "yes" || checklowerCase ==="y"){
       checklowerCase = true;
       return checklowerCase;
+// if no, checklowerCase is false
     }else if (checklowerCase === "no" || checklowerCase ==="n"){
       checklowerCase = false;
       return checklowerCase;
+// if anything else, ask again
     }else {
       alert("Please answer Yes or No");
       lowercaseCheck();
@@ -76,59 +81,71 @@ function lowercaseCheck(){
 function uppercaseCheck(){
   checkUpperCase = prompt("Do you want uppercase in your password? \n(Yes or No)");
     checkUpperCase = checkUpperCase.toLowerCase();
+// asks for response of yes or no
     if (checkUpperCase === null || checkUpperCase === ""){
       alert("Please answer Yes or No");
       uppercaseCheck();
+// if yes, checkUpperCase is true
     }else if (checkUpperCase === "yes" || checkUpperCase ==="y"){
       checkUpperCase = true;
       return checkUpperCase;
+// if no, checkUpperCase is false
     }else if (checkUpperCase === "no" || checkUpperCase ==="n"){
       checkUpperCase = false;
       return checkUpperCase;
+// if anything else, ask again
     }else {
       alert("Please answer Yes or No");
       uppercaseCheck();
     }
     return checkUpperCase;
   }
-  // asks if you want numbers in your password 
-  function numberCheck(){
-    checkNumber = prompt("Do you want numbers in your password? \n(Yes or No)");
-      checkNumber = checkNumber.toLowerCase();
-        if (checkNumber === null || checkNumber === ""){
-        alert("Please answer Yes or No");
-        numberCheck();
-        }else if (checkNumber === "yes" || checkNumber ==="y"){
-        checkNumber = true;
-        return checkNumber;
-        }else if (checkNumber === "no" || checkNumber ==="n"){
-        checkNumber = false;
-        return checkNumber;
-       }else {
-        alert("Please answer Yes or No");
-        numberCheck();
-      }
+// asks if you want numbers in your password 
+function numberCheck(){
+  checkNumber = prompt("Do you want numbers in your password? \n(Yes or No)");
+    checkNumber = checkNumber.toLowerCase();
+// asks for response of yes or no
+    if (checkNumber === null || checkNumber === ""){
+      alert("Please answer Yes or No");
+       numberCheck();
+// if yes, checkNumber is true
+    }else if (checkNumber === "yes" || checkNumber ==="y"){
+      checkNumber = true;
       return checkNumber;
+// if no, checkNumber is false
+    }else if (checkNumber === "no" || checkNumber ==="n"){
+      checkNumber = false;
+      return checkNumber;
+// if anything else, ask again 
+    }else {
+      alert("Please answer Yes or No");
+      numberCheck();
     }
+    return checkNumber;
+  }
 
     // asks if you want special characters in your password
-  function specialCheck(){
-    checkSpecial = prompt("Do you want special in your password? \n(Yes or No)");
-      checkSpecial = checkSpecial.toLowerCase();
-      if (checkSpecial === null || checkSpecial === ""){
-        alert("Please answer Yes or No");
-        specialCheck();
-      }else if (checkSpecial === "yes" || checkSpecial ==="y"){
-        checkSpecial = true;
-        return checkSpecial;
-      }else if (checkSpecial === "no" || checkSpecial ==="n"){
-        checkSpecial = false;
-        return checkSpecial;
-      }else {
-        alert("Please answer Yes or No");
-        specialCheck();
-      }
+function specialCheck(){
+  checkSpecial = prompt("Do you want special characters in your password? \n(Yes or No)");
+    checkSpecial = checkSpecial.toLowerCase();
+// asks for response of yes or no
+    if (checkSpecial === null || checkSpecial === ""){
+      alert("Please answer Yes or No");
+      specialCheck();
+// if yes, checkSpecial is true
+    }else if (checkSpecial === "yes" || checkSpecial ==="y"){
+      checkSpecial = true;
       return checkSpecial;
+
+    }else if (checkSpecial === "no" || checkSpecial ==="n"){
+      checkSpecial = false;
+      return checkSpecial;
+// if anything else, ask again
+    }else {
+      alert("Please answer Yes or No");
+      specialCheck();
+    }
+    return checkSpecial;
   }
 
 
@@ -145,47 +162,44 @@ function generatePassword(){
   console.log(checkSpecial); // shows true & false when yes or no is selected - works
 
   var characterString = ""
-
-
+// adding var password its not in global scope.
+  var password = "";
 
   // credit: https://stackoverflow.com/a/32625608 SiCK (allowed me to figure out check each against each other)
-  var password = "";
+  // checks if checklowerCase and upperCase and number and special are true or false
   if (checklowerCase && checkUpperCase && checkNumber && checkSpecial){
     characterString += lowercaseChar + uppercaseChar + numberChar + specialChar;
-  //              1               2
+  
   } else if (checklowerCase && checkUpperCase){
     characterString += lowercaseChar + uppercaseChar;
-//               2                  3
+
   } else if (checkUpperCase && checkNumber){
     characterString += uppercaseChar + numberChar;
-//               3                 4
+
   } else if (checkNumber && checkSpecial){
     characterString += numberChar + specialChar;
-//               4                 1
+
   } else if (checkSpecial && checklowerCase){
     characterString += specialChar + lowercaseChar;
-//              1                                          
+                                        
   } else if (checklowerCase){
     characterString += lowercaseChar;
-//              2
+
   } else if (checkUpperCase){
     characterString += uppercaseChar;
-//              3
+
   } else if (checkNumber){
     characterString += numberChar;
-//              4
+
   } else if (checkSpecial){
     characterString += specialChar;
   }
 
-
+// for loop to generate password
   for (var i = 0; i < passwordLength; i++){
     password += characterString.charAt(Math.floor(Math.random() * characterString.length));
   }
-
+  // returns password to main function
   return password;
-
-
-
 }
 
