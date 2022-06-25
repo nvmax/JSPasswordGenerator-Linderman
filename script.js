fd36e1076b61cedccf34ba2626db2145388e75d8
Jerrod Linderman <nvmaxx@gmail.com>
2022-06-25 11:33:48 -0500

    replicated function for each option length,lowecase,uppercase,number and special, checked in function all passed

--- script.js
+++ script.js
@@ -40,8 +40,97 @@ function lengthCheck() {
   return passwordLength;
 } 
 
+// asks if you want lowercase letters
+function lowercaseCheck(){
+  checklowerCase = prompt("Do you want lowercase in your password? \n(Yes or No)");
+    checklowerCase = checklowerCase.toLowerCase();
+    if (checklowerCase === null || checklowerCase === ""){
+      alert("Please answer Yes or No");
+      lowercaseCheck();
+    }else if (checklowerCase === "yes" || checklowerCase ==="y"){
+      checklowerCase = true;
+      return checklowerCase;
+    }else if (checklowerCase === "no" || checklowerCase ==="n"){
+      checklowerCase = false;
+      return checklowerCase;
+    }else {
+      alert("Please answer Yes or No");
+      lowercaseCheck();
+    }
+    return checklowerCase;
+ }
+
+// asks if you want uppercase letters
+function uppercaseCheck(){
+  checkUpperCase = prompt("Do you want uppercase in your password? \n(Yes or No)");
+    checkUpperCase = checkUpperCase.toLowerCase();
+    if (checkUpperCase === null || checkUpperCase === ""){
+      alert("Please answer Yes or No");
+      uppercaseCheck();
+    }else if (checkUpperCase === "yes" || checkUpperCase ==="y"){
+      checkUpperCase = true;
+      return checkUpperCase;
+    }else if (checkUpperCase === "no" || checkUpperCase ==="n"){
+      checkUpperCase = false;
+      return checkUpperCase;
+    }else {
+      alert("Please answer Yes or No");
+      uppercaseCheck();
+    }
+    return checkUpperCase;
+  }
+  // asks if you want numbers in your password 
+  function numberCheck(){
+    checkNumber = prompt("Do you want numbers in your password? \n(Yes or No)");
+      checkNumber = checkNumber.toLowerCase();
+        if (checkNumber === null || checkNumber === ""){
+        alert("Please answer Yes or No");
+        numberCheck();
+        }else if (checkNumber === "yes" || checkNumber ==="y"){
+        checkNumber = true;
+        return checkNumber;
+        }else if (checkNumber === "no" || checkNumber ==="n"){
+        checkNumber = false;
+        return checkNumber;
+       }else {
+        alert("Please answer Yes or No");
+        numberCheck();
+      }
+      return checkNumber;
+    }
+
+    // asks if you want special characters in your password
+  function specialCheck(){
+    checkSpecial = prompt("Do you want special in your password? \n(Yes or No)");
+      checkSpecial = checkSpecial.toLowerCase();
+      if (checkSpecial === null || checkSpecial === ""){
+        alert("Please answer Yes or No");
+        specialCheck();
+      }else if (checkSpecial === "yes" || checkSpecial ==="y"){
+        checkSpecial = true;
+        return checkSpecial;
+      }else if (checkSpecial === "no" || checkSpecial ==="n"){
+        checkSpecial = false;
+        return checkSpecial;
+      }else {
+        alert("Please answer Yes or No");
+        specialCheck();
+      }
+      return checkSpecial;
+  }
+
+
 function generatePassword(){
   lengthCheck();
-  console.log(passwordLength); // RETURED 9 WORKS
+  console.log(passwordLength); // shows # entered - works
+  lowercaseCheck();
+  console.log(checklowerCase); // shows if lowercase is selected - works
+  uppercaseCheck();
+  console.log(checkUpperCase); // shows true & false when yes or no is selected - works
+  numberCheck();
+  console.log(checkNumber); // shows true & false when yes or no is selected - works
+  specialCheck();
+  console.log(checkSpecial); // shows true & false when yes or no is selected - works
+
 }
 
