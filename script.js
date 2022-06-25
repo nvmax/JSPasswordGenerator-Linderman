bbf2e3f007a4786a582513874e4617aff59c6823
Jerrod Linderman <nvmaxx@gmail.com>
2022-06-25 10:39:49 -0500

    created variables

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// creating variables length, lowercase, uppercase, numeric, special
+var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberChar = "0123456789";
+var specialChar = "!@#$%^&*()_-+={}[];:'`~<,>.?/|";
+var passwordLength;
+var checklowerCase;
+var checkUpperCase;
+var checkNumber;
+var checkSpecial;
 
 // Write password to the #password input
 function writePassword() {
